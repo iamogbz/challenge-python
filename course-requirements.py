0f881f140db48865cd4d3a411b8a314989934613
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2018-10-27 14:54:59 -0400

    Course requirements (#1)
    * wip
    
    * wip
    
    * wip

---
+++ course-requirements.py
@@ -0,0 +1,60 @@
+def solve(courses):
+    """
+    Analyse courses for possibility of completion
+    :param courses: list of courses [requirement, auxilliary]
+    """
+    print("---")
+    c2r = dict() # course to requirements
+    r2c = dict() # requirement to courses
+    for [r, c] in courses:
+        if r not in r2c:
+            r2c[r] = set()
+        r2c[r].add(c)
+        if c not in c2r:
+            c2r[c] = set()
+        c2r[c].add(r)
+    # courses with no requirements
+    n_courses = r2c.keys() - c2r.keys()
+    v_courses = set()
+    count = 0
+    while n_courses:
+        p_courses = set()
+        for r in n_courses:
+            for c in r2c.get(r, []):
+                count += 1
+                c2r[c].remove(r)
+                if not c2r[c]:
+                    p_courses.add(c)
+        v_courses.update(n_courses)
+        n_courses = p_courses
+
+    print(count, v_courses, c2r)
+    return v_courses == set().union(r2c.keys(), c2r.keys())
+
+
+# some tests
+print(solve([[1, 0], [2, 0], [2, 1]]), True)
+print(solve([[1, 0], [0, 2], [2, 1], [3, 1]]), False)
+print(solve([[1, 0], [2, 0], [2, 1], [3, 1]]), True)
+print(solve([[1, 0], [2, 0], [3, 1], [1, 2]]), True)
+print(solve([[1, 0], [2, 0], [3, 0], [2, 1], [3, 1], [3, 2]]), True)
+print(
+    solve(
+        [
+            [1, 0],
+            [2, 0],
+            [3, 0],
+            [4, 0],
+            [2, 1],
+            [3, 1],
+            [4, 1],
+            [3, 2],
+            [4, 2],
+            [5, 2],
+            [4, 3],
+            [5, 4],
+        ]
+    ),
+    True,
+)
+print(solve([[1, 0], [3, 2], [5, 4], [7, 6], [8, 9], [11, 10]]), True)
