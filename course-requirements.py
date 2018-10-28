17dae5d44278e744ee1f61b324b0cbb898a844d6
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2018-10-28 14:19:57 -0400

    feat: remove comments

--- course-requirements.py
+++ course-requirements.py
@@ -3,7 +3,6 @@ def solve(courses):
     Analyse courses for possibility of completion
     :param courses: list of courses [requirement, auxilliary]
     """
-    # print("---")
     c2r = dict() # course to requirements
     r2c = dict() # requirement to courses
     for [r, c] in courses:
@@ -13,27 +12,17 @@ def solve(courses):
         if c not in c2r:
             c2r[c] = set()
         c2r[c].add(r)
-    # courses with no requirements can be taken
+
     n_courses = r2c.keys() - c2r.keys()
-    v_courses = set()
-    count = 0
     while n_courses:
-        # next set of courses that can be taken
         p_courses = set()
         for r in n_courses:
-            # get courses that required this one
             for c in r2c.get(r, []):
-                count += 1
-                # remove this course from pending requirements
                 c2r[c].remove(r)
-                # if course has no more requirements
                 if not c2r[c]:
-                    # add to set of courses to take next
                     p_courses.add(c)
-        v_courses.update(n_courses)
         n_courses = p_courses
 
-    # print(count, v_courses, c2r)
     return not sum(map(len, c2r.values()))
 
 
