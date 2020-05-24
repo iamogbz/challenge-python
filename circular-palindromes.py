299694b95861688c8b372fab6c601e9beb8efc73
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-05-23 22:03:56 -0400

    wip: add test cases

--- circular-palindromes.py
+++ circular-palindromes.py
@@ -5,47 +5,42 @@ import time
 from typing import List
 
 
-def all_palindromes(word: str, size: int) -> List[List[int]]:
+def all_palindromes(word: str, size: int, limit: int):
     """
-    Gets the position and max size of all palidromic substrings
+    Gets the position and max size of all palidromic substrings based on
     https://en.wikipedia.org/wiki/Longest_palindromic_substring#Manacher's_algorithm
-
-    Arguments:
-        word -- the word to analyse
-        size -- the length of the word
-
-    Returns:
-        list of list of integers -- where each index is the center position
-        of the palidrome, and each integer is the length from the center to
-        the left and right to make the palidrome. A value of zero means a no
-        palidrome of maximum length greater than 1 was found. With the second
-        being the odd length palindromes to be offset by 1 on the input word
-        i.e. word[i - 1 - maximum : i - 1 + maximum + s]
-    
-    Example:
-        Arguments: ("aabadd", 6)
-        Returns: [
-            [0, 0, 1, 0, 0, 0, 1], // evens
-            [0, 0, 0, 1, 0, 0, 0], // odds
-        ]
     """
     bounded_word = "@" + word + "#"
-    radii: List[List[int]] = [[0] * (size + 1) for _ in range(2)]
+    radii = [[0] * (size + 1) for _ in range(2)]
     for s in range(2):
-        maximum = 0
+        longest = 0
         i = 1
         while i <= size:
-            while bounded_word[i - 1 - maximum] == bounded_word[i + maximum + s]:
-                maximum += 1
-            radii[s][i] = maximum
+            while (
+                bounded_word[i - 1 - longest] == bounded_word[i + longest + s]
+                and longest * 2 + s < limit
+            ):
+                longest += 1
+            radii[s][i] = longest
             k = 1
-            while radii[s][i - k] != (maximum - k) and k < maximum:
-                radii[s][i + k] = min(radii[s][i - k], maximum - k)
+            while radii[s][i - k] != (longest - k) and k < longest:
+                radii[s][i + k] = min(radii[s][i - k], longest - k)
                 k += 1
-            maximum = max(maximum - k, 0)
+            longest = max(longest - k, 0)
             i += k
 
-    return radii
+    return [
+        (m * 2 + s, i - 1 - m)
+        for s, r in enumerate(radii)
+        for i, m in enumerate(r)
+        if m * 2 + s > 1
+    ]
+
+
+def circular_palindromes(word: str, size: int):
+    palindromes = all_palindromes(word + word[:-1], size * 2 - 1, size)
+    palindromes.sort(reverse=True)
+    yield from []
 
 
 # =============================================================================
@@ -55,22 +50,24 @@ def all_palindromes(word: str, size: int) -> List[List[int]]:
 if __name__ == "__main__":
     N = int(input())
     S = input()
-    time_a = time.time()
-    all_palindromes(S, N)
-    time_b = time.time()
-    print("time: {} secs".format(round(time_b - time_a, 3)))
+    for cycle_longest in circular_palindromes(S, N):
+        print(cycle_longest)
 
 
 # =============================================================================
 # TESTS
 # -----------------------------------------------------------------------------
-def test_all_palindromes():
-    word = "happytotwilling"
-    radii = all_palindromes(word, len(word))
-    palindromes = sorted(
-        word[i - 1 - m : i - 1 + m + s]
-        for s, r in enumerate(radii)
-        for i, m in enumerate(r)
-        if m > 0
-    )
-    assert palindromes == ["illi", "pp", "tot"]
+import pytest
+
+
+@pytest.mark.parametrize("test_case", [15, 25])
+def test_circular_palindromes(test_case):
+    with open(f"circular-palindromes_input{test_case}.txt", "r") as f:
+        size = int(f.readline())
+        word = f.readline()
+    time_a = time.perf_counter()
+    with open(f"circular-palindromes_output{test_case}.txt", "r") as f:
+        for n in circular_palindromes(word, size):
+            assert str(n) == f.readline().strip()
+    time_b = time.perf_counter()
+    print("time: {} secs".format(round(time_b - time_a, 3)))
