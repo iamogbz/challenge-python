3e40154c843786c1c27ac20e9bf77e7e141bd359
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-05-24 10:20:23 -0400

    wip: timed test cases

--- circular-palindromes.py
+++ circular-palindromes.py
@@ -2,6 +2,7 @@
 # https://leetcode.com/problems/longest-palindromic-substring/solution/
 
 import time
+from collections import defaultdict
 from typing import List
 
 
@@ -12,6 +13,7 @@ def all_palindromes(word: str, size: int, limit: int):
     """
     bounded_word = "@" + word + "#"
     radii = [[0] * (size + 1) for _ in range(2)]
+    palindromes = defaultdict(set)
     for s in range(2):
         longest = 0
         i = 1
@@ -21,26 +23,43 @@ def all_palindromes(word: str, size: int, limit: int):
                 and longest * 2 + s < limit
             ):
                 longest += 1
+                if longest * 2 + s > 1:
+                    palindromes[longest * 2 + s].add(i - 1 - longest)
             radii[s][i] = longest
             k = 1
             while radii[s][i - k] != (longest - k) and k < longest:
                 radii[s][i + k] = min(radii[s][i - k], longest - k)
+                if radii[s][i + k] * 2 + s > 1:
+                    palindromes[radii[s][i + k] * 2 + s].add(
+                        (i + k) - 1 - radii[s][i + k]
+                    )
                 k += 1
             longest = max(longest - k, 0)
             i += k
 
-    return [
-        (m * 2 + s, i - 1 - m)
-        for s, r in enumerate(radii)
-        for i, m in enumerate(r)
-        if m * 2 + s > 1
-    ]
+    return palindromes
 
 
 def circular_palindromes(word: str, size: int):
     palindromes = all_palindromes(word + word[:-1], size * 2 - 1, size)
-    palindromes.sort(reverse=True)
+    # lengths = sorted(palindromes.keys(), reverse=True)
+    skip = set()
     yield from []
+    return
+    for rotation in range(size):
+        longest = 1
+        for length in range(size, 1, -1):
+            for start in palindromes[length]:
+                if (length, start) in skip:
+                    continue
+                if start <= rotation:
+                    skip.add((length, start))
+                if start >= rotation and start + length <= rotation + size:
+                    longest = length
+                    break
+            if longest > 1:
+                break
+        yield longest
 
 
 # =============================================================================
@@ -61,6 +80,7 @@ import pytest
 
 
 @pytest.mark.parametrize("test_case", [15, 25])
+@pytest.mark.timeout(10)
 def test_circular_palindromes(test_case):
     with open(f"circular-palindromes_input{test_case}.txt", "r") as f:
         size = int(f.readline())
