772a1b86a0f31782f8f2d1323cd6d95df67ae784
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2023-11-21 16:51:08 -0500

    wip: add naive bubble sort implementation

--- common_algos.py
+++ common_algos.py
@@ -33,7 +33,36 @@ def quick_sort(s: "Sequence"):
         )
 
 
-def bubble_sort(q):
+def bubble_sort(s: "Sequence", gap: "int" = 1):
+    """
+    Bubble sort algorithm
+    Worst case time: O(N^2)
+    Average case time: O(NlogN)
+
+    [prefer insertion sort]
+    Starting from one end of the sequence and moving to the opposite end
+    Pick the next two values and swap them into correct positions
+    Move to the next two values and repeat until no swaps are needed
+
+    :return: sequence sorted in ascending order
+    """
+    q = list(s)  # consume all elements of sequence
+    # print([q])
+    n = len(q)  # count final number of elements
+
+    for l in range(n - gap, gap - 1, -1):
+        did_swap = False
+        for i in range(l):
+            a = q[i]
+            b = q[i + gap]
+            if a > b:
+                did_swap = True
+                q[i] = b
+                q[i + gap] = a
+
+        if not did_swap:
+            break
+
     return q
 
 
@@ -74,5 +103,12 @@ def test_quick_sort():
         assert quick_sort(tc) == sorted(tc)
 
 
+def test_bubble_sort():
+    """Validate bubble sort against python default sorting"""
+    for tc in SORTING_TESTCASES:
+        assert bubble_sort(tc) == sorted(tc)
+
+
 # run tests
 test_quick_sort()
+test_bubble_sort()
